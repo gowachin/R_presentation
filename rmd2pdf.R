fb7fbc22b3726b5559a3cce76c8e66659deb1dc2
gowachin <gowachin974@gmail.com>
2022-03-17 00:34:12 +0100

    fork modif on CSU

--- rmd2pdf.R
+++ rmd2pdf.R
@@ -8,6 +8,10 @@ xaringan:::inf_mr(moon = "CSU_RepRo.Rmd")
 # Rendering to pdf ####
 
 pagedown::chrome_print("CSU_RepRo.html")
-
+# install.packages("remotes")
+remotes::install_github("jhelvy/xaringanBuilder")
 library(xaringanBuilder) # not working because missing chrome
 # build_pdf("CSU_RepRo.html")
+
+install.packages('sass')
+install.packages('xaringan')
