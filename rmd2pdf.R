fc67389e251825d72cdd57d14bd3b4e688cd8da5
gowachin <maxime.jaunatre@yahoo.fr>
2023-01-23 14:34:38 +0100

    updtae scrip

--- rmd2pdf.R
+++ rmd2pdf.R
@@ -8,8 +8,8 @@ xaringan:::inf_mr(moon = "CSU_RepRo.Rmd")
 xaringan:::inf_mr(moon = "git_exo.Rmd")
 
 ## Rendering to pdf ####
-# remotes::install_github("jhelvy/xaringanBuilder")
+# remotes::install_github("jhelvy/renderthis")
 # also require pdftools and chromote
-library(xaringanBuilder) # not working because missing chrome
-build_pdf("CSU_RepRo.html", partial_slides = TRUE)
+library(renderthis) # not working because missing chrome
+to_pdf("CSU_RepRo.html", partial_slides = TRUE)
 to_pdf("Tuto_git.html", partial_slides = TRUE)
